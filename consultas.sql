1de0871b0def4ae4fad95f53869ba8cc735936c5
EDGARD <edgard.rodriguez@vallegrande.edu.pe>
2023-07-08 20:32:35 -0500

    cambios data base

--- consultas.sql
+++ consultas.sql
@@ -1,9 +1,8 @@
 USE RSU_ASISTENCIA;
-SELECT * FROM ASESOR;
-select * from PROYECTO_DETALLE;
+SELECT * FROM ASISTENCIA;
+select * from PROYECTO_DETALLE ;
 select id,nombre from PROYECTOS where estado ='A' OR estado='E';
-
- 
+select * from PROYECTO_DETALLE where estudiantes_fk=2; 
  select * from ESTUDIANTES WHERE rol_fk=1;
-INSERT INTO PROYECTO_DETALLE (descripcion,proyectos_fk,estudiantes_fk,evidencia) VALUES (?,?,?,?)
-update PROYECTO_DETALLE set descripcion=?,proyectos_fk=?,estudiantes_fk=?,evidencia=? where id=?
+INSERT INTO ASISTENCIA (dia,cantHoras,fecha,estado,estudiantes_fk,proyecto_detalle_fk) VALUES (?,?,?,?,?,?)
+update ASISTENCIA set dia=?,cantHoras=?,fecha=?,estado=?,estudiantes_fk=?,proyecto_detalle_fk=? where id=?
